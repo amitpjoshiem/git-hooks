f6b6a42897e5695b1109b3a34de9057a95fb8056
Amit Joshi <Amit.Joshi@engineersmind.com>
2023-11-01 02:06:56 +0530

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
-    parserOptions: {
-        ecmaVersion: 14,
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: true,
-        },
+  parserOptions: {
+    ecmaVersion: 14,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: ["react"],
-    extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
-    //overwriting below rule with existing rules
-    rules: {
-//        semi: ["error", "never"],
-        //"means semi colun should no be use"
-        //"always", //"means semi colun should no be use"
-
-    },
+  },
+  plugins: ["react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  //overwriting below rule with existing rules
+  rules: {
+    //        semi: ["error", "never"],
+    //"means semi colun should no be use"
+    //"always", //"means semi colun should no be use"
+  },
 }
