4404a14dd952cae2d253e51b98bcb99bb79ff8c2
Amit Joshi <Amit.Joshi@engineersmind.com>
2023-11-01 01:40:31 +0530

    testing

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-import { foo } from "./Test";
-import React from "react";
-class Car extends React.Component {
-  render() {
-    return <h2>Hi, I am a Car!</h2>;
-  }
-}
-var age = 20;
+import { foo } from "./Test"
+
+var age = 20
+
 function hello(age) {
-  return foo + "hello" + age;
+  return foo + "hello" + age
 }
-hello(age);
+
+hello(age)
